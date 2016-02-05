a401dcbff0a8a6037899a47ce7036cd1a02eae4a
juanoh <juanoh@unitec.edu>
2016-02-05 15:47:40 -0600

    ejercicio1

---
+++ Lab.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void menuFun();
+void ejercicio1Resp(int);
+int ejercicio1RespIntentos(int,int);
+void arrayOrdering(int[],int,int);
+
+int main(int argc, char const *argv[])
+{
+	int const arrayIntentosSizeMain=10;
+	int const arraySTATSSizeMainFILAS=4;
+	int const arraySTATSSizeMainCOL=3;
+	int const arraySTATSJUGSizeMainFILAS=2;
+	int const arraySTATSJUGSizeMainCOL=3;
+	int arraySTATSJUG[arraySTATSJUGSizeMainFILAS][arraySTATSJUGSizeMainCOL];
+	int arrayScore[arrayIntentosSizeMain]={100,100,100,100,100,100,100,100,100,100};
+	int arraySTATS[arraySTATSSizeMainFILAS][arraySTATSSizeMainCOL];
+	menuFun();
+	int respMEnMain, numeroIntentoMain, numeroResultadoMain,numeroScore=0;
+	char respNuevoJuegoEjer1;
+	cin>>respMEnMain;
+	do
+	{
+		if (respMEnMain==1){
+			//Ejercicio 1
+			srand(time(NULL));
+			int numeroAzar = rand()%1001-500;
+			cout<<numeroAzar<<endl;
+			do
+			{	
+				cout<<"Ingrese un Número"<<endl;
+				cin>>numeroIntentoMain;
+				numeroResultadoMain= ejercicio1RespIntentos(numeroIntentoMain,numeroAzar);
+				ejercicio1Resp(numeroResultadoMain);
+				numeroScore++;
+			} while (numeroResultadoMain!=3);
+			cout<<"Felicidades, solo te tomo "<<numeroScore<<" intentos "<<endl;
+			arrayOrdering(arrayScore,arrayIntentosSizeMain,numeroScore);
+			cout<<"Otro Juego? [y/n]: "<<endl;
+			for (int i = 0; i < arrayIntentosSizeMain; ++i)
+			{
+				cout<<"--"<<arrayScore[i]<<"--"<<endl;
+			}
+			numeroScore=0;
+			cin>>respNuevoJuegoEjer1;
+			if (respNuevoJuegoEjer1=='n'){
+				cout<<"Score No Guardado"<<endl;
+				break;
+			}
+		}else if (respMEnMain ==2){
+			srand(time(NULL));
+			int numeroAzarATK = rand()%30+85;
+			int numeroAzarDEF = rand()%25+50;
+			int numeroAzarSPE = rand()%50+150;
+
+		}else{
+			cout<<"Numero Inválido!!"<<endl;
+		}
+	} while (respMEnMain!=3);
+	return 0;
+}
+
+void menuFun(){
+	cout<<"1-Mayor o Menor\n2-Juego2\n3-Salir"<<endl;
+}
+
+void ejercicio1Resp(int numeroResultadoMain){
+	if (numeroResultadoMain==1)
+	{
+		cout<<"EL Número es Menor"<<endl;
+	}else if (numeroResultadoMain==2){
+		cout<<"El Número es Mayor"<<endl;
+	}else if (numeroResultadoMain==3){
+		cout<<"Correcto"<<endl;
+	}
+}
+int ejercicio1RespIntentos(int numIntento, int numeroAzar){
+	if (numIntento>numeroAzar)
+	{
+		return 1;
+	}else if (numIntento<numeroAzar){
+		return 2;
+	}else if (numIntento==numeroAzar){
+		return 3;
+	}
+}
+
+void arrayOrdering(int arrayScoreFun[],int arrayIntentosSizeFun,int numeroScoreFun){
+	int numRempTmp, cambioNum=1;
+	for (int i = 0; (i < arrayIntentosSizeFun)&&cambioNum==1; ++i)
+	{
+		if (numeroScoreFun < arrayScoreFun[i] || numeroScoreFun==arrayScoreFun[i])
+		{
+			numRempTmp = arrayScoreFun[i];
+			arrayScoreFun[i]=numeroScoreFun;
+			arrayScoreFun[i+1]=numRempTmp;
+			cambioNum=0;
+		}
+	}
+}
